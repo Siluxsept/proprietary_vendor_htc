1c9d3e44ad5e4b865792d3a230588d5dede1b20d
Siluxsept <ronny.horn.rh@gmail.com>
2021-03-31 00:15:33 +0200

    oce: drop libhtc_sensorhub.so
    Change-Id: Ib0e59b57e42f29b6cbb5e96b6af6d4e991fff0b3

--- oce/oce-vendor.mk
+++ oce/oce-vendor.mk
@@ -233,7 +233,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/oce/proprietary/lib/libftm_lib_sensorhub.so:system/lib/libftm_lib_sensorhub.so \
     vendor/htc/oce/proprietary/lib/libhtc_humanprocess.so:system/lib/libhtc_humanprocess.so \
     vendor/htc/oce/proprietary/lib/libhtc_facebeauty.so:system/lib/libhtc_facebeauty.so \
-    vendor/htc/oce/proprietary/lib/libhtc_sensorhub.so:system/lib/libhtc_sensorhub.so \
     vendor/htc/oce/proprietary/lib/libmdsprpc.so:system/lib/libmdsprpc.so \
     vendor/htc/oce/proprietary/lib/libmorpho_edit_engine.so:system/lib/libmorpho_edit_engine.so \
     vendor/htc/oce/proprietary/lib/libmorpho_frame_overlay.so:system/lib/libmorpho_frame_overlay.so \
@@ -262,7 +261,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/oce/proprietary/lib64/lib-imsvtutils.so:system/lib64/lib-imsvtutils.so \
     vendor/htc/oce/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
     vendor/htc/oce/proprietary/lib64/libftm_lib_sensorhub.so:system/lib64/libftm_lib_sensorhub.so \
-    vendor/htc/oce/proprietary/lib64/libhtc_sensorhub.so:system/lib64/libhtc_sensorhub.so \
     vendor/htc/oce/proprietary/product/lib64/libimscamera_jni.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libimscamera_jni.so \
     vendor/htc/oce/proprietary/product/lib64/libimsmedia_jni.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libimsmedia_jni.so \
     vendor/htc/oce/proprietary/lib64/libmdsprpc.so:system/lib64/libmdsprpc.so \
